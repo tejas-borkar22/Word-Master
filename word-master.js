3c933f8fb868a5623a9883ac65f69a1d4aaab1d4
Tejas Borkar <76744372+tejas-borkar22@users.noreply.github.com>
2022-10-30 11:49:35 +0530

    feat : Added win /lose condition

--- word-master.js
+++ word-master.js
@@ -1,16 +1,20 @@
 const letters = document.querySelectorAll('.board-letter')
 const loadingSpinner = document.querySelector('.info-bar')
 const ANSWER_LENGTH = 5
+const ROUNDS = 6
 
 async function init() {
   let currentGuess = ""
   let currentRow = 0
+  let done = false
+  let isLoading = true
 
   // Fetching the word of the day from api
   const response = await fetch("https://words.dev-apis.com/word-of-the-day")
   const responseObj = await response.json()
   const word = responseObj.word.toUpperCase()
   setLoadingGif(false)
+  isLoading = false
   const wordParts = word.split("")
   console.log(word)
 
@@ -28,18 +32,42 @@ async function init() {
   
     // Validate the word
     const guessParts = currentGuess.split("")
+    // console.log(guessParts)
     const map = makeMap(wordParts)
-    console.log(map)
-
+    // console.log(map)
+    
     for(let i=0;i<ANSWER_LENGTH;i++){
+      // Marking it as correct
       if(guessParts[i] === wordParts[i]){
         letters[currentRow * ANSWER_LENGTH +i].classList.add("correct")
         map[guessParts[i]]--
-        console.log(map[guessParts[i]])
       }
     } 
 
+    for(let i=0;i<ANSWER_LENGTH;i++){
+      if(guessParts[i] === wordParts[i]){
+        //Do nothing already did it.
+      }
+      // Marking it as close
+      else if(wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0){
+        letters[currentRow * ANSWER_LENGTH +i].classList.add("close")
+        map[guessParts[i]]--
+      }
+      else
+      letters[currentRow * ANSWER_LENGTH +i].classList.add("wrong")
+    }
+
     currentRow++
+    // Win or lose logic
+    if(currentGuess === word){
+      console.log("You win 🥇")
+      done = true
+      return
+    }else if(currentRow === ROUNDS){
+      console.log(`The word of the day was ${word}`)
+      done = true 
+    }
+
     currentGuess = ""
   }
 
@@ -49,8 +77,10 @@ async function init() {
   }
 
   document.addEventListener('keydown', function handleKeyPress(event) {
+    if(done || isLoading){
+      return // Do nothing
+    }
     const action = event.key
-
     if(action === 'Enter')
       commitWord()
     else if(action === 'Backspace')
